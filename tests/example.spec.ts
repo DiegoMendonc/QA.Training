61b9eadc16a1da081f1a5aaf8aaca6f917f57293
DiegoMendonc <diego_felipe_g.m@live.com>
2024-12-05 23:42:42 -0300

    toDo Test - Playwright
    Início do projeto - CRUD Test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,38 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('CRUD de To-Do List', () =>
+    // Antes de cada teste, navegue até a URL base da aplicação
+    test.beforeEach(async ({ page }) => {
+      await page.goto('http://localhost:3000/Todo-List#/all-tasks')
+    }));
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+    //1º Teste -> Criando uma nova tarefa - CREATE
+    test('Deve criar uma nova tarefa', async ({ page }) => {
+        await page.fill('#new-todo-input', 'Comprar leite');
+        await page.click('#add-todo-button');
+        const todoText = await page.textContent('.todo-item:last-child');
+        expect(todoText).toBe('Comprar leite');
+    });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+    //2º Teste -> Visualizar a tarefa adicionada - READ
+    test('Deve ler a tarefa adicionada', async ({ page }) => {
+        const todoText = await page.textContent('.todo-item:last-child');
+        expect(todoText).toBe('Comprar leite');
+    });
+    
+    //3º Teste -> Deverá atualizar a tarefa adicionada - UPDATE
+    test('Deve atualizar uma tarefa', async ({ page }) => {
+        await page.click('.todo-item:last-child .edit-button');
+        await page.fill('.todo-item:last-child .edit-input', 'Comprar pão');
+        await page.click('.todo-item:last-child .save-button');
+        const todoText = await page.textContent('.todo-item:last-child');
+        expect(todoText).toBe('Comprar pão');
+    });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+    //4º Teste -> Deverá deletar a tarefa criada - DELETE
+    test('Deve deletar uma tarefa', async ({ page }) => {
+        await page.click('.todo-item:last-child .delete-button');
+        const todoItems = await page.$$('.todo-item');
+        expect(todoItems.length).toBe(0);
+    });
+);
