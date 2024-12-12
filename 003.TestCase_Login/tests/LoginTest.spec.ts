dc091fbe9b8cb8513e04b9695dd96e419e98f151
DiegoMendonc <diego_felipe_g.m@live.com>
2024-12-11 23:40:23 -0300

    Update LoginTest.spec.ts

--- 003.TestCase_Login/tests/LoginTest.spec.ts
+++ 003.TestCase_Login/tests/LoginTest.spec.ts
@@ -3,22 +3,23 @@ import { LoginCredential } from './LoginCredential';
 
 test.describe('Login - Swag Labs', () => {
   
-  test.beforeEach(async ({ page }) => {
-    const loginPage = new LoginCredential(page);
-    await loginPage.goto();
-  });
-
-  test('Correct Credentials - Login Test', async ({ page }) => {
-    const loginCredential = new LoginCredential(page);
-
-    await loginCredential.login('standard_user', 'secret_sauce');
-    expect(await page.url()).toBe('https://www.saucedemo.com/v1/inventory.html');
+    test.beforeEach(async ({ page }) => {
+        const loginPage = new LoginCredential(page);
+        await loginPage.goto();
     });
 
-  test('Incorrect Credentials - Login test', async ({ page }) => {
-    const loginCredential = new LoginCredential(page);
-    await loginCredential.login('usuario_invalido', 'senha_invalida');
+    test('Correct Credentials - Login Test', async ({ page }) => {
+        const loginCredential = new LoginCredential(page);
 
-    expect(await page.locator('data-test="error"')).toBe('Username and password do not match any user in this service');
-  });
+        await loginCredential.login('standard_user', 'secret_sauce');
+        expect(await page.url()).toBe('https://www.saucedemo.com/v1/inventory.html');
+    });
+
+    test('Incorrect Credentials - Login test', async ({ page }) => {
+        const loginCredential = new LoginCredential(page);
+        await loginCredential.login('usuario_invalido', 'senha_invalida');
+        const errorMessage = await loginCredential.getErrorMessage();
+        expect(errorMessage).toBe('Epic sadface: ');
+    });
 });
+
