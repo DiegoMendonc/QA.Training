49f17641e30eb2ffc53c13955ce305d183aed466
DiegoMendonc <diego_felipe_g.m@live.com>
2024-12-13 11:51:18 -0300

    update

--- 003.TestCase_Login/playwright.config.ts
+++ 003.TestCase_Login/playwright.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, devices } from '@playwright/test';
-
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -12,12 +11,11 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: '/003.TestCase_Login/tests',
-  timeout: 30 * 1000,
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly:!!process.env.CI,
+  forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
@@ -26,13 +24,13 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    screenshot: "on",
-    video: "on",
+    screenshot: 'on',
+    video: 'on',
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    //baseURL: 'http://localhost:3000/Todo-List#/all-tasks',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
@@ -72,7 +70,6 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
@@ -80,3 +77,5 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
+
+
--- 003.TestCase_Login/tests/LoginTest.spec.ts
+++ 003.TestCase_Login/tests/LoginTest.spec.ts
@@ -1,6 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('Deve logar com credenciais válidas', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/v1/');
-    
+
+    await expect(page.getByText('Swag Labs')).toBeVisible();
 });
